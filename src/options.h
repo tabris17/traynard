484595c0e5c2f9b5b52944122af1bd656305d4c2
fournoas <admin@fournoas.com>
2025-01-10 18:29:02 +0800

    Remove deprecated static strings

--- src/options.h
+++ src/options.h
@@ -1,15 +1,4 @@
-//
 #pragma once
-
-/*#define TEXT_TRAY _T("托盘图标")
-#define TEXT_MENU _T("右键菜单")
-#define TEXT_HOTKEY_HIDE _T("隐藏窗口热键")
-#define TEXT_HOTKEY_MENU _T("菜单热键")
-#define TEXT_COL_KEY _T("按键")
-#define TEXT_COL_ACTION _T("行为")
-#define TEXT_ACT_1 _T("最小化前台窗口")
-#define TEXT_ACT_2 _T("弹出最小化窗口列表")
-#define TEXT_ACT_3 _T("还原最后一个窗口")*/
 
 INT_PTR showOptionsDlg(TRCONTEXT* context);
 void loadOptions(TRCONTEXT* context);
